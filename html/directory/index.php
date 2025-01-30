886cb7523ea94fdfda2acf47e000795b1041b158
James Pederson <james@jpederson.com>
2025-01-30 20:11:08 +0000

    Add access control header.

--- html/directory/index.php
+++ html/directory/index.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
 // include the configuration
 require( '../config.php' );
 
--- html/is-search/index.php
+++ html/is-search/index.php
@@ -1,5 +1,7 @@
 <?php 
 
+header('Access-Control-Allow-Origin: *');
+
 require( '../config.php' );
 
 // interpret query vars
